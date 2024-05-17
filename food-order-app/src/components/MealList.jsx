2c215914a6950e268f946278cfb2ddba4bd908cf
Agnieszka Szkurlat <javalodzdev@gmail.com>
2024-05-17 12:30:16 +0200

    Custom hook useHttp.js and Error component added

--- food-order-app/src/components/MealList.jsx
+++ food-order-app/src/components/MealList.jsx
@@ -1,24 +1,22 @@
-import { useState, useEffect } from "react";
-
+import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
 import Meal from "./Meal.jsx";
 
+const requestConfig = {};
+
 export default function MealList() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-
-      if (!response.ok) {
-        // ...
-      }
-
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
+  }
 
   return (
     <ul id="meals">
