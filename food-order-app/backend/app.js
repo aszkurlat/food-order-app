93d55dc4410715ef28865b357ef1b241b4883fae
Agnieszka Szkurlat <javalodzdev@gmail.com>
2024-05-18 21:37:21 +0200

    backend update

--- food-order-app/backend/app.js
+++ food-order-app/backend/app.js
@@ -1,13 +1,13 @@
-import fs from "fs/promises";
-import path from "path";
+import fs from "node:fs/promises";
+
 import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
-// set headers CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
@@ -15,67 +15,57 @@ app.use((req, res, next) => {
   next();
 });
 
-// path to files JSON
-const dataPath = path.join(process.cwd(), "data");
-
-// Endpoint for meals
-app.get("/api/meals", async (req, res) => {
-  try {
-    const meals = await fs.readFile(
-      path.join(dataPath, "available-meals.json"),
-      "utf8"
-    );
-    res.json(JSON.parse(meals));
-  } catch (error) {
-    console.error("Error reading meals data:", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+app.get("/meals", async (req, res) => {
+  const meals = await fs.readFile("/data/available-meals.json", "utf8");
+  res.json(JSON.parse(meals));
 });
 
-// Endpoint for orders
-app.post("/api/orders", async (req, res) => {
+app.post("/orders", async (req, res) => {
   const orderData = req.body.order;
 
-  if (!orderData || !orderData.items || orderData.items.length === 0) {
+  if (
+    orderData === null ||
+    orderData.items === null ||
+    orderData.items === []
+  ) {
     return res.status(400).json({ message: "Missing data." });
   }
 
-  const requiredFields = ["email", "name", "street", "postal-code", "city"];
-  const missingFields = requiredFields.filter(
-    (field) => !orderData.customer[field]
-  );
-
-  if (missingFields.length > 0) {
+  if (
+    orderData.customer.email === null ||
+    !orderData.customer.email.includes("@") ||
+    orderData.customer.name === null ||
+    orderData.customer.name.trim() === "" ||
+    orderData.customer.street === null ||
+    orderData.customer.street.trim() === "" ||
+    orderData.customer["postal-code"] === null ||
+    orderData.customer["postal-code"].trim() === "" ||
+    orderData.customer.city === null ||
+    orderData.customer.city.trim() === ""
+  ) {
     return res.status(400).json({
-      message: `Missing data: ${missingFields.join(", ")} is missing.`,
+      message:
+        "Missing data: Email, name, street, postal code or city is missing.",
     });
   }
 
-  try {
-    const orders = await fs.readFile(
-      path.join(dataPath, "orders.json"),
-      "utf8"
-    );
-    const allOrders = JSON.parse(orders);
-    const newOrder = { ...orderData, id: (Math.random() * 1000).toString() };
-    allOrders.push(newOrder);
-    await fs.writeFile(
-      path.join(dataPath, "orders.json"),
-      JSON.stringify(allOrders)
-    );
-    res.status(201).json({ message: "Order created!" });
-  } catch (error) {
-    console.error("Error writing order data:", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+  const newOrder = {
+    ...orderData,
+    id: (Math.random() * 1000).toString(),
+  };
+  const orders = await fs.readFile("/data/orders.json", "utf8");
+  const allOrders = JSON.parse(orders);
+  allOrders.push(newOrder);
+  await fs.writeFile("/data/orders.json", JSON.stringify(allOrders));
+  res.status(201).json({ message: "Order created!" });
 });
 
-// Obsługa innych ścieżek
 app.use((req, res) => {
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
+
   res.status(404).json({ message: "Not found" });
 });
 
-export default app;
+app.listen(3000);
