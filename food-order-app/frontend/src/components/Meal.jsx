f644bf939290b7b7ba38450f84f44d88321e269c
Agnieszka Szkurlat <javalodzdev@gmail.com>
2024-05-30 12:54:35 +0200

    frontend  update

--- food-order-app/frontend/src/components/Meal.jsx
+++ food-order-app/frontend/src/components/Meal.jsx
@@ -15,7 +15,7 @@ export default function MealItem({ meal }) {
     <li className="meal">
       <article>
         <img
-          src={`https://food-order-app-backend-eight.vercel.app/${meal.image}`}
+          src={`https://food-order-app-backend-eight.vercel.app/images/${meal.image}`}
           // src={`http://localhost:3000/${meal.image}`}
           alt={meal.name}
         />
